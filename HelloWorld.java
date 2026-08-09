68499d9a7fba25239bf59edc46fce363f2df02ee
Divyajain15 <divyaofficial1507@gmail.com>
2026-08-09 16:09:42 +0530

    only little changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld
 {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        //this is refference
+        System.out.println("Hi my Name is Divya");
     }
 }
